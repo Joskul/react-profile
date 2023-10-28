710cd9f838b1600455f59e4bf4a5395bd725d2d0
Schpudt <hikaru.kp@gmail.com>
2023-10-28 14:53:38 +0700

    +Navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import profilePic from "../assets/pic.png";
+import { FaArrowRightLong } from "react-icons/fa";
+
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { FaTimes, FaBars } from "react-icons/fa";
+import ReactTypingEffect from "react-typing-effect";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const menu = [
+    {
+      id: 1,
+      link: "home",
+    },
+    {
+      id: 2,
+      link: "about",
+    },
+    {
+      id: 3,
+      link: "Experience",
+    },
+    {
+      id: 4,
+      link: "contact",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-fg fixed bg-bg_s">
+      <div className="flex w-full">
+        {/* <h1 className="text-5xl font-signature ml-2">Kurvidah</h1> */}
+        <ReactTypingEffect
+          text="Kurvidah"
+          className="text-5xl font-signature ml-2"
+          speed="100"
+          cursor="_"
+        />
+      </div>
+      <ul className="hidden md:flex">
+        {menu.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-fg3 hover:text-fg transition-all"
+          >
+            {link}
+          </li>
+        ))}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-fg3 hover:text-fg transition-all md:hidden"
+      >
+        {nav ? <FaTimes size="30" /> : <FaBars size="30" />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-bg_h to-bg_s">
+          {menu.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl text-fg3 hover:text-5xl hover:text-fg transition-all"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default NavBar;
